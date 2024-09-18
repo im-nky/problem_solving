e135dc9ceb571883762690a9f4fde962be5d195a
Nitin Kumar Yadav <im_nky@yahoo.com>
2024-09-18 15:26:34 +0530

    set_nums_in_range.c

---
+++ bit_manupluation/set_nums_in_range.c
@@ -0,0 +1,27 @@
+// C++ implementation to Set bits in
+// the given range
+#include <iostream>
+using namespace std;
+ 
+// function to toggle bits in the given range
+int setallbitgivenrange(int n, int l, int r)
+{
+    // calculating a number 'range' having set
+    // bits in the range from l to r and all other
+    // bits as 0 (or unset).
+    cout << "\nnumber made by shifting left" << ((1 << (l - 1)) - 1);
+    cout << "\nnumber made by shifting Right" << ((1 << (r)) - 1)<<endl;
+
+    int range = (((1 << (l - 1)) - 1) ^    
+                ((1 << (r)) - 1));
+ 
+    return (n | range);
+}
+ 
+// Driver code
+int main()
+{
+    int n = 17, l = 2, r = 3;
+    cout << setallbitgivenrange(n, l, r);
+    return 0;
+}
