06e1a2bd9a2a0f490d788e7a5f2875c8f0721dde
Nitin Kumar Yadav <im_nky@yahoo.com>
2024-12-05 09:01:42 +0530

    write_to_byte.c

--- bit_manupluation/write_to_byte.c
+++ bit_manupluation/write_to_byte.c
@@ -13,18 +13,20 @@ int main()
     return 0;
 }
 
-#include <stdio.h>
-#include <stdint.h>
 int main()
 {
-    int a;
+    int a[2]; // index of array starts with 0 but size starts with 1
     char * ptr;
     ptr = (uint8_t *)&a;
     *(ptr+0) = 1;
     *(ptr+1) = 4;
     *(ptr+2) = 5;
     *(ptr+3) = 7;
-    for(uint8_t i=0;i<=3;i++) {
+    *(ptr+4) = 9;
+    *(ptr+5) = 10;
+    *(ptr+6) = 11;
+    *(ptr+7) = 12;
+    for(uint8_t i=0;i<=7;i++) {
         printf("%d\t",*(ptr+i));
     }
     return 0;
