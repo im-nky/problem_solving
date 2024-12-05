2d3ca342027d34d745088f3f3e45b5b27083e9fe
Nitin Kumar Yadav <im_nky@yahoo.com>
2024-12-05 08:57:38 +0530

    write_to_byte.c

--- bit_manupluation/write_to_byte.c
+++ bit_manupluation/write_to_byte.c
@@ -1,7 +1,6 @@
 // Write to each byte or memset
 #include <stdio.h>
 #include <stdint.h>
-
 int main()
 {
     int a=10;
@@ -9,6 +8,23 @@ int main()
     ptr = (uint8_t *)&a;
     for(uint8_t i=0;i<=3;i++) {
         *(ptr+i) = 1;
+        printf("%d\t",*(ptr+i));
+    }
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdint.h>
+int main()
+{
+    int a;
+    char * ptr;
+    ptr = (uint8_t *)&a;
+    *(ptr+0) = 1;
+    *(ptr+1) = 4;
+    *(ptr+2) = 5;
+    *(ptr+3) = 7;
+    for(uint8_t i=0;i<=3;i++) {
         printf("%d\t",*(ptr+i));
     }
     return 0;
