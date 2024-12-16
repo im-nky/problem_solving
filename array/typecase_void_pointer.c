499d3269dc8b5860301c5005ad15da4d612d9a29
Nitin Kumar Yadav <im_nky@yahoo.com>
2024-12-16 10:44:01 +0530

    typecase_void_pointer.c

---
+++ array/typecase_void_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    void *ptr = &num; // Assigning the address of num to a void pointer
+
+    // To access the value, you need to cast the void pointer back to an int pointer
+    int * intPtr = (int *)ptr;
+    printf("Value: %d\n", *intPtr);
+
+    return 0;
+}
