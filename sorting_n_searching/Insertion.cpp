6db9c5c5b55c8f0ced798ec40fa51bf2ef0d3841
Nitin Kumar Yadav <im_nky@yahoo.com>
2025-02-13 08:17:20 +0530

    insertion_sort.cpp

--- sorting_n_searching/Insertion.cpp
+++ sorting_n_searching/Insertion.cpp
@@ -4,9 +4,7 @@ using namespace std;		//Insertion sort
 int main()
 {
 	int i,key,j;
-	//int arr[]={213,234,2,412,4532,432,4,3,43,5};
-	int arr[10];	
-	cout<<"Enter Values in array\n";
+	int arr[]={213,234,2,412,4532,432,4,3,43,5};
 	for(i=0;i<10;i++)	
 	{
 		cin>>arr[i];
