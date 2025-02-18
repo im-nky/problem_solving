4c442a8f11d079305fb831ee222c68a9d752e3c8
Nitin Kumar Yadav <im_nky@yahoo.com>
2025-02-18 16:33:43 +0530

    print_null_pointers.c

--- array/print_null_array.c
+++ array/print_null_array.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int * a = NULL;  
+    int * a = NULL;
+    char * s = NULL;
     printf("%d\n", *a); // prints nothing
     printf("%p\n", a); // prints NULL
+    printf("%p\n", s);  // prints null
+    printf("%s\n", s);    // nothing
+    printf("%c\n", *s);
     return 0;
 }
